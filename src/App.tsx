ab04c80f8c4acebc55d0b672947933363fc33660
vanthonglee <thonglevan131@gmail.com>
2022-09-05 17:18:52 +0700

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
-import ProTip from "./ProTip";
+import React from 'react'
+import { Provider } from 'react-redux'
+import { HistoryRouter as Router } from 'redux-first-history/rr6'
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
+import 'App.css'
+import AppRoutes from 'routes'
+import { history, store } from 'store/store'
 
-export default function App() {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App example with TypeScript
-        </Typography>
-        <ProTip />
-        <div>Input Search Component</div>
-        <div>Result List</div>
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+const App = () => (
+  <>
+    <Provider store={store}>
+      <Router history={history}>
+        <AppRoutes />
+      </Router>
+    </Provider>
+  </>
+)
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,22 @@
-import ReactDOM from "react-dom/client";
-import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
-import App from "./App";
-import theme from "./theme";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// import reportWebVitals from "./reportWebVitals";
+import App from 'App'
+import 'config/i18n'
+import 'index.css'
+import reportWebVitals from 'reportWebVitals'
+import { initMockServiceWorker } from 'test/msw'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
-root.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
+initMockServiceWorker()
+
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </ThemeProvider>,
-);
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals()
